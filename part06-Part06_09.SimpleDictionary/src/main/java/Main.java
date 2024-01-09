763fc641bca31602dfce867d12ac98d42d9baebd
RichiCod3s <aaronrichardsonsg@gmail.com>
2024-01-09 20:57:24 +0800

    Commit

--- part06-Part06_09.SimpleDictionary/src/main/java/Main.java
+++ part06-Part06_09.SimpleDictionary/src/main/java/Main.java
@@ -4,7 +4,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // Try out how the different parts of the program work together
-        // here
+     
+        //create new scanner and SimpleDictionary class
+        Scanner scanner = new Scanner(System.in);
+        SimpleDictionary dictionary = new SimpleDictionary();
+        
+        // create a new TextUI class 
+        TextUI textUI = new TextUI(scanner, dictionary);
+        
+        textUI.start();
+        
+        //System.out.println(dictionary.translate("pike"));
+        
     }
 }

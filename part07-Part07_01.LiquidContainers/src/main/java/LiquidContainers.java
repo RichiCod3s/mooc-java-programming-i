bc447cafefb093439a5fb8636df86d6f740c0bfe
RichiCod3s <aaronrichardsonsg@gmail.com>
2024-01-13 22:51:13 +0800

    Part 6 completed
    Started briefly on Part 7-1

--- part07-Part07_01.LiquidContainers/src/main/java/LiquidContainers.java
+++ part07-Part07_01.LiquidContainers/src/main/java/LiquidContainers.java
@@ -6,13 +6,33 @@ public class LiquidContainers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        int firstContainer = 0;
+        int secondContainer = 0;
 
         while (true) {
-            System.out.print("> ");
+            System.out.println("First: "+firstContainer+"/100");
+            System.out.println("Second: "+secondContainer+"/100");
+            
+            System.out.println("> ");
 
             String input = scan.nextLine();
             if (input.equals("quit")) {
                 break;
+            }
+
+            String[] parts = input.split(" ");
+
+            String command = parts[0];
+            int amount = Integer.valueOf(parts[1]);
+
+            if (command.equals("add")) {
+                if (firstContainer + amount > 100) {
+                    firstContainer = 100;
+                } else {
+                     if(!(amount < 0)){
+                    firstContainer += amount;
+                }
+                }
             }
 
         }

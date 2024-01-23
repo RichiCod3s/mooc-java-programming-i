9a27d0e8f45e984bba87573e566e51dc36e3fe79
RichiCod3s <aaronrichardsonsg@gmail.com>
2024-01-23 21:09:33 +0800

    7/4 complete

--- part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
+++ part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
@@ -1,10 +1,47 @@
 
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
 
 public class Main {
 
     public static void main(String[] args) {
         // insert test code here
         int[] array = {3, 1, 5, 99, 3, 12};
+        String[] animals = {"Dog","Cat","Bee","Crocodile"};
+        sort(array);
+        sort(animals);
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(animals));
+        
+        ArrayList<String> birds = new ArrayList<>();
+        
+        birds.add("Robin");
+        birds.add("Magpie");
+        birds.add("Ostrich");
+        sortStrings(birds);
+        
+        System.out.println(birds);
+        
     }
 
+    public static void sort(int[] array){
+        Arrays.sort(array);
+    } 
+    
+    public static void sort(String[] array){
+    Arrays.sort(array);
+    }
+    
+    public static void sortIntegers(ArrayList<Integer> integers){
+        Collections.sort(integers);
+    }
+    
+    public static void sortStrings(ArrayList<String> strings){
+         Collections.sort(strings);
+    }
+    
+    
 }
